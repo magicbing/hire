692febab3a174834734aa5f9e97df9ab30b13d76
magicbing <magicbingl@foxmail.com>
2020-03-26 22:59:02 +0800

    add mocha test

--- foundations-zh/src/index.ts
+++ foundations-zh/src/index.ts
@@ -9,5 +9,33 @@ export interface ErrorMessage {
 
 export function parseError(err: Error): ErrorMessage {
   // implement
-  // git add
+  
+  const regMatch:RegExp = /(http:\/\/|https:\/\/).+?js.*?:\d+:\d+/gs
+  const regFilename:RegExp = /(http:\/\/|https:\/\/).+?js/gs
+  const regNumber:RegExp = /(?<=:)\d+/gs
+
+  const matchArr:RegExpMatchArray = err.message.match(regMatch) || []
+  const stack = matchArr && matchArr.map(str => {
+    const lineArr:RegExpMatchArray = str.match(regNumber) || []
+    const line:number = parseInt(lineArr.slice(-2,-1)[0])
+
+    const columnArr:RegExpMatchArray = str.match(regNumber) || []
+    const column:number = parseInt(columnArr && columnArr.slice(-1)[0])
+
+    const filenameArr:RegExpMatchArray | null = str.match(regFilename)
+    const filename:string = filenameArr && filenameArr[0] || ''
+    
+    return {
+      line,
+      column,
+      filename
+    }
+  })
+
+  const result = {
+    message: 'Error raised',
+    stack
+  }
+  
+  return result
 }
---
+++ foundations-zh/tests/indextest.ts
@@ -0,0 +1,68 @@
+import {
+  parseError
+} from '../src/index';
+
+const {
+  describe,
+  it
+} = require('mocha');
+
+const {
+  expect
+} = require('chai');
+
+const fixtureStack = new Error(`TypeError: Error raised
+at bar http://192.168.31.8:8000/c.js:2:9
+at foo http://192.168.31.8:8000/b.js:4:15
+at calc http://192.168.31.8:8000/a.js:4:3
+at <anonymous>:1:11
+at http://192.168.31.8:8000/a.js:22:3
+`)
+
+const fixtureFirefoxStack = new Error(`
+bar@http://192.168.31.8:8000/c.js:2:9
+foo@http://192.168.31.8:8000/b.js:4:15
+calc@http://192.168.31.8:8000/a.js:4:3
+<anonymous>:1:11
+http://192.168.31.8:8000/a.js:22:3
+`)
+
+const except = {
+  message: 'Error raised',
+  stack: [
+    {
+      line: 2,
+      column: 9,
+      filename: 'http://192.168.31.8:8000/c.js'
+    },
+    {
+      line: 4,
+      column: 15,
+      filename: 'http://192.168.31.8:8000/b.js'
+    },
+    {
+      line: 4,
+      column: 3,
+      filename: 'http://192.168.31.8:8000/a.js'
+    },
+    {
+      line: 22,
+      column: 3,
+      filename: 'http://192.168.31.8:8000/a.js'
+    },
+  ]
+}
+
+describe('#parseError test', () => {
+  describe('chrome', () => {
+    it('chrome pass', () => {
+      expect(parseError(fixtureStack)).to.deep.equal(except);
+    })
+  })
+
+  describe('firefox', () => {
+    it('firefox pass', () => {
+      expect(parseError(fixtureFirefoxStack)).to.deep.equal(except);
+    })
+  })
+})
